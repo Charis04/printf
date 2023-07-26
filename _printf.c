f9adf11cab324d12d92a75e8fa53930aa8a939cf
Mohammed Karoud <m.karoud@outlook.com>
2023-07-26 16:00:47 -0700

    handle strings

--- _printf.c
+++ _printf.c
@@ -39,7 +39,7 @@ int _printf(const char *format, ...)
 				str = va_arg(ap, char*);
 				if (str == NULL)
 				{
-					format++;
+					format+=2;
 					continue;
 				}
 				while (str[len] != '\0')
